1442d096c8ec393c55add05d94cc91adc661047f
HegedusJanos <janos22245@freemail.hu>
2023-09-24 12:19:01 +0200

    Asd

---
+++ Konyvtar/Konyvtar/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+namespace Konyvtar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Asd");
+        }
+    }
+    public class Books
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string AName { get; set; }
+        public string PublishingPlace { get; set; }
+        public int PublihingYear { get; set; }
+
+    }
+    public class Reader
+    { 
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int BirthYear { get; set; }
+    }
+
+}
